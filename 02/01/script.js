f417953dccc543d88eb675326f22f986f034e837
William <william.williamson.91@gmail.com>
2021-02-03 15:53:03 +0100

    calculator

--- 02/01/script.js
+++ 02/01/script.js
@@ -1,42 +1,39 @@
-/* becode/javascript
- *
- * /02-maths/01-calculator-one/script.js - 2.1: calculatrice
- *
- * coded by leny@BeCode
- * started at 26/10/2018
- */
-
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
-
 (function() {
     // to get the value of an input: document.getElementById("element-id").value
 
     document.getElementById("addition").addEventListener("click", function() {
-        // perform an addition
+        const number1 = document.getElementById("op-one").value;
+        const number2 = document.getElementById("op-two").value;
+
+        let result = parseFloat(number1) + parseFloat(number2);
+
+        window.alert(result);
     });
 
     document.getElementById("substraction").addEventListener("click", function() {
-        // perform an substraction
+        const number1 = document.getElementById("op-one").value;
+        const number2 = document.getElementById("op-two").value;
+
+        let result = number1 - number2;
+
+        window.alert(result);
     });
 
     document.getElementById("multiplication").addEventListener("click", function() {
-        // perform an multiplication
+        const number1 = document.getElementById("op-one").value;
+        const number2 = document.getElementById("op-two").value;
+
+        let result = number1 * number2;
+
+        window.alert(result);
     });
 
     document.getElementById("division").addEventListener("click", function() {
-        // perform an division
-    });
-    if (operator == '+') {
-        result = number1 + number2;
-    } else if (operator == '-') {
-        result = number1 - number2;
-    } else if (operator == '*') {
-        result = number1 * number2;
-    } else {
-        result = number1 / number2;
-    }
+        const number1 = document.getElementById("op-one").value;
+        const number2 = document.getElementById("op-two").value;
 
-    // display the result
-    console.log(`${number1} ${operator} ${number2} = ${result}`);
+        let result = number1 / number2;
+
+        window.alert(result);
+    });
 })();
