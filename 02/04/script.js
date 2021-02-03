d90b7f644e04ef49fdf2f05c506e5f30236b71a4
William <william.williamson.91@gmail.com>
2021-02-03 17:57:37 +0100

    square

---
+++ 02/04/script.js
@@ -0,0 +1,22 @@
+/* becode/javascript
+ *
+ * /02-maths/03-even-square/script.js - 2.3: carrés des pairs
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+
+    document.getElementById("run").addEventListener("click", function() {
+
+        // your code here
+        for (var x = 1; x < 22; x++) {
+            console.log(Math.pow(x, 2));
+        }
+    });
+
+})();
