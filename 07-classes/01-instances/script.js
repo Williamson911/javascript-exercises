c5f50c5d67a7d688765b2763f669724aa307aff2
William <william.williamson.91@gmail.com>
2021-02-22 09:54:56 +0100

    classes

--- 07-classes/01-instances/script.js
+++ 07-classes/01-instances/script.js
@@ -16,5 +16,8 @@
             this.age = age;
         }
     }
-    // your code here
+    document.getElementById("run").addEventListener("click", () => {
+        console.log(new Cat("Skitty", 9));
+        console.log(new Cat("Pixel", 6));
+    });
 })();
