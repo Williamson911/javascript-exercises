c0457266b9f38402210483918488579b316b51b1
William <william.williamson.91@gmail.com>
2021-02-24 11:18:47 +0100

    errors

--- 10-asynchronous/03-handling-errors/script.js
+++ 10-asynchronous/03-handling-errors/script.js
@@ -10,5 +10,17 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+
+    document.getElementById("run").addEventListener("click", () => {
+
+        window.lib.getPersons((error, people) => {
+            if (error) {
+                console.error(error);
+            } else {
+                console.log(people);
+            }
+        });
+
+    });
+
 })();
