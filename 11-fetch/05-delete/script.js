e536c7aadbd84e489a91c674bd6eccffd66bf77d
William <william.williamson.91@gmail.com>
2021-02-25 14:36:39 +0100

    fetchdel

--- 11-fetch/05-delete/script.js
+++ 11-fetch/05-delete/script.js
@@ -10,5 +10,37 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+    async function getHero() {
+        try {
+            const hero = await fetch('http://localhost:3000/heroes');
+            const data = await hero.json();
+            return data;
+        } catch (error) {
+            console.error(error);
+        };
+    }
+
+    let array = getHero();
+
+    document.getElementById('run').addEventListener('click', function() {
+        function killHero(data) {
+            const input = document.querySelector('#hero-id').value;
+
+            data.forEach((element, i) => {
+
+                if (element.id == input) {
+                    data.splice(i, 1);
+                }
+            });
+
+            if (input.length == 0) {
+                alert('there seems to be an empty input');
+            }
+        };
+
+        array.then(data => {
+            killHero(data);
+            console.log(data);
+        })
+    });
 })();
