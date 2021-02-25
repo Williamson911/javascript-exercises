9858b43d6ca2ed23e191f49fcef72e56fb5577c1
William <william.williamson.91@gmail.com>
2021-02-25 09:31:54 +0100

    asyncomerr

--- 10-asynchronous/08-async-get-comments/script.js
+++ 10-asynchronous/08-async-get-comments/script.js
@@ -10,5 +10,45 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+
+    const commentsFetched = 0;
+
+    const asyncGetComments = async(postId, getCommentsFunction) => {
+        const commentsPromise = await getCommentsFunction(postId);
+        try {
+            return commentsPromise;
+        } catch (e) {
+            console.error(e);
+            return null;
+        }
+    }
+
+    const asyncGetPosts = async(getPostsFunction) => {
+        const postsPromise = await getPostsFunction();
+        try {
+            postsPromise.forEach((post) => {
+                post.comment = asyncGetComments(post.id, window.lib.getComments);
+            });
+            return postsPromise;
+        } catch (e) {
+            console.error(e);
+            return null;
+        }
+    }
+
+    const printMessage = async() => {
+        const message = await asyncGetPosts(window.lib.getPosts);
+        try {
+            console.log(message);
+        } catch (e) {
+            console.error(e);
+        }
+    };
+
+    document.getElementById("run").addEventListener("click", () => {
+
+        printMessage();
+
+    });
+
 })();
